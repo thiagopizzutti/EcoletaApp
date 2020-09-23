01893464cc229543b0798404e11073fba0a6ffa3
Thiago Pizzutti <tpizzutti@gmail.com>
2020-09-23 11:41:58 -0300

    buid: Link installed

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import {FiLogIn} from 'react-icons/fi'
 
 import logo from '../../assets/logo.svg'
@@ -17,12 +18,12 @@ const Home = () => {
             <h1>Seu marketplace de coleta de resíduos</h1>
             <p>Ajudamos pessoas a encontrarem pontos de coleta de formaficiente</p>
 
-            <a href="/cadastro">
+            <Link to="/create-point">
               <span>
                 <FiLogIn />
               </span>
               <strong>Cadastre um ponto de coleta</strong>
-            </a>
+            </Link>
           </main>
 
         </div>
